a9cbaaf039308db0881c2fc289e88a3b16cec518
James <98997059+jamesfsher@users.noreply.github.com>
2024-03-10 19:12:44 -0500

    Removed pygame functionality, going to do in terminal instead

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+def main():
+    ...
+    # Call select word to randomly select word
+    # Create # of trys counter
+    # Create loop that displays current progress towards word (initalize as empty)
+    # prompt user for guess
+    # Error check guess to make sure its a valid entry
+    # compare entry against currently stored word
+    # if a letter is correct, and in the correct location, display it capitalized
+    # if a letter is correct but in the wrong location, make it lower case
+    # if word is correct, diplay so
+    # if word is not correct still, reprompt with updated progress and try counter++
+    # possibly display all the possible letters also, eliminating the incorrectly guessed ones?
+
+    
+
+def select_word():
+    ...
+    # Function to select a word from a long list of words (create as separate text file?)
+    # Randomly select a word
+    # Store as a variable in current session
+
+
+
+
+
+if __name__ == '__main__':
+    main()
