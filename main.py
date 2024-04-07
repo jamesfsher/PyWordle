6a5f4ca11987bcb4d22d812ee8795904cc7bef47
James <98997059+jamesfsher@users.noreply.github.com>
2024-04-07 17:49:22 -0500

    working on converting to csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 
 def main():
     ...
@@ -17,13 +18,9 @@ def main():
     
 def txt_to_csv():
     with open('list_of_words.txt', 'r') as in_file:
-        stripped = (line.strip() for line in in_file)
-        lines = (line.split(" ") for line in stripped if line) 
-        with open('list_of_words.csv', 'w') as out_file:
-            writer = csv.writer(out_file) 
-            writer.writerow(lines)
-
-
+        lines = in_file.readlines()
+    list_of_words = pd.read_csv("list_of_words.txt", sep=r'\s+')
+ 
 
 def select_word():
     ...
