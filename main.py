919c9e52918c9f6385ae717d9236de346a663e81
James <98997059+jamesfsher@users.noreply.github.com>
2024-04-15 20:34:34 -0500

    created proper csv from txt, select word fucntion working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import random
 
 def main():
     ...
@@ -14,14 +15,28 @@ def main():
     # if word is correct, diplay so
     # if word is not correct still, reprompt with updated progress and try counter++
     # possibly display all the possible letters also, eliminating the incorrectly guessed ones?
-    
- 
+    print(select_word())
+
+
+    # Code that created csv from .txt list of words
+    # new_list = []
+    # with open('list_of_words.txt', 'r') as file:
+    #     words = file.read().splitlines()
+
+    # with open('output_file.csv', 'w', newline='') as csvfile:
+    #     writer = csv.writer(csvfile)
+    #     for word in words:
+    #         writer.writerow([word])
 
 def select_word():
     ...
     # Function to select a word from a long list of words (create as separate text file?)
     # Randomly select a word
     # Store as a variable in current session
+    with open('list_of_words.csv', 'r') as file:
+        reader = csv.reader(file)
+        return random.choice(list(reader))
+    
 
 
 
