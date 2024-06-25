973db867cef7206d5985bf33cbb869d7923d8819
James <98997059+jamesfsher@users.noreply.github.com>
2024-06-25 16:48:05 -0500

    removed project desc notes at top

--- project.py
+++ project.py
@@ -5,18 +5,6 @@ import os
 import string
 
 def main():
-    ...
-    # Call select word to randomly select word
-    # Create # of trys counter
-    # Create loop that displays current progress towards word (initalize as empty)
-    # prompt user for guess
-    # Error check guess to make sure its a valid entry
-    # compare entry against currently stored word
-    # if a letter is correct, and in the correct location, display it capitalized
-    # if a letter is correct but in the wrong location, make it lower case
-    # if word is correct, diplay so
-    # if word is not correct still, reprompt with updated progress and try counter++
-    # possibly display all the possible letters also, eliminating the incorrectly guessed ones?
     print("""
           Welcome to PyWordle!
           Play the game by entering a 5 letter word to try to match one randomly chosen.
@@ -29,7 +17,6 @@ def main():
 
 
 def play_round():
-    ...
     # Function to play and track a round of 5 
     current_word, all_words = select_word()
     current_word = "apple"
@@ -92,7 +79,6 @@ def play_round():
 
 
 def end_game(result):
-    ...
     # function that takes a "win" or "lose" variable, and ends the game and resets globals to new game mode
     if result == "winner":
         print("you've won!")
@@ -103,7 +89,6 @@ def end_game(result):
 
 
 def select_word():
-    ...
     # Randomly select a word from csv file containing 5 letter words
     words = []
     with open('list_of_words.csv', 'r') as file:
